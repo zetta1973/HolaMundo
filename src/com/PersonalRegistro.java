c5f25121283038ea101c61820ff8c6d1bb03db21
Vicente <vicente.garcia@outlook.com>
2025-11-14 17:17:04 +0100

    Se agrego una linea que no vale para nada

--- src/com/PersonalRegistro.java
+++ src/com/PersonalRegistro.java
@@ -4,6 +4,7 @@ public record PersonalRegistro (String nombre, String apellidos){
 
   @Override
   public String toString() {
+    System.out.println("Esta instruccion es inservible");
     return "PersonalRegistro{" +
         "nombre='" + nombre + '\'' +
         ", apellidos='" + apellidos + '\'' +
