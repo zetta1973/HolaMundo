9d8936e15ef797f7380ced57730b37b15ea74809
Vicente <vicente.garcia@outlook.com>
2025-11-14 10:24:44 +0100

    Se integró la clase Mensaje

--- src/com/acme/HolaMundo.java
+++ src/com/acme/HolaMundo.java
@@ -3,6 +3,8 @@ package com.acme;
 public class HolaMundo {
 
   public static void main(String[] args) {
-    System.out.println("Hola Mundo !!");
+    Mensaje msg = new Mensaje();
+    String nombre = msg.solicitaDatos("Escribe tu nombre: ");
+    msg.saludar(nombre);
   }
 }
